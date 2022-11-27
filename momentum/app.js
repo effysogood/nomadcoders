1d5dececaec049ace4d62de62db66dbea1a481c3
sy.choi <sy.choi@salin.co.kr>
2022-11-27 18:29:44 +0900

    update notes

---
+++ momentum/app.js
@@ -0,0 +1,13 @@
+const player = {
+    firstName: "effy",
+    age: 28,
+    nationality: "Republic Of Korea",
+    gender: "Female",
+    ranking: 1,
+}
+
+console.log(player);
+console.log(player.gender);
+player.score = "1400";
+player.ranking = "14";
+console.log(player);
