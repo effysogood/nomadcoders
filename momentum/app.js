58c50d3fefca54a1aae8ce64f47083116b4de4a6
sy.choi <sy.choi@salin.co.kr>
2022-11-29 17:19:18 +0900

    #STIDY 3.5

--- momentum/app.js
+++ momentum/app.js
@@ -1,13 +1,43 @@
-const player = {
-    firstName: "effy",
-    age: 28,
-    nationality: "Republic Of Korea",
-    gender: "Female",
-    ranking: 1,
+const header = document.querySelector(".header");
+const title = document.querySelector(".header:first-child h1");
+
+console.dir(title);
+
+function handleTitleClick() {
+    title.style.color = "white";
 }
 
-console.log(player);
-console.log(player.gender);
-player.score = "1400";
-player.ranking = "14";
-console.log(player);
+function handleMouseEnter() {
+    title.innerText = "Mouse is here!";
+}
+
+function handleMouseLeave() {
+    title.innerText = "Mouse is gone!";
+}
+
+function handleWindowResize() { 
+    document.body.style.backgroundColor = "tomato";
+}
+
+function handleWindowCopy() {
+    alert("Copier!");
+}
+
+function handleWindowOnline() {
+    alert("SOS! NO WIFI!");
+}
+
+function handleWindowOffline() {
+    alert("ALL GOOD!");
+}
+
+title.addEventListener("click", handleTitleClick);
+//title.onClick = handleTitleClick;
+title.addEventListener("mouseenter", handleMouseEnter);  
+title.addEventListener("mouseleave", handleMouseLeave);  
+
+window.addEventListener("resize", handleWindowResize);
+window.addEventListener("copy", handleWindowCopy);
+window.addEventListener("offline", handleWindowOffline);
+window.addEventListener("offline", handleWindowOnline);
+window.addEventListener("offline", handleWindowOffline);
