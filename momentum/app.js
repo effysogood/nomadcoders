cd0d6900d6439f2e854d19f279023b000537b931
sy.choi <sy.choi@salin.co.kr>
2022-11-29 19:50:27 +0900

    #3.8 CSS in Javascript part Three

--- momentum/app.js
+++ momentum/app.js
@@ -1,43 +1,17 @@
 const header = document.querySelector(".header");
 const title = document.querySelector(".header:first-child h1");
 
-console.dir(title);
-
 function handleTitleClick() {
-    title.style.color = "white";
+  title.classList.toggle("clicked");
 }
 
-function handleMouseEnter() {
-    title.innerText = "Mouse is here!";
-}
-
-function handleMouseLeave() {
-    title.innerText = "Mouse is gone!";
-}
-
-function handleWindowResize() { 
-    document.body.style.backgroundColor = "tomato";
-}
-
-function handleWindowCopy() {
-    alert("Copier!");
-}
-
-function handleWindowOnline() {
-    alert("SOS! NO WIFI!");
-}
-
-function handleWindowOffline() {
-    alert("ALL GOOD!");
-}
+// function handleTitleClick() {
+//   const clickedClass = "clicked";
+//   if (title.classList.contains(clickedClass)) {
+//     title.classList.remove(clickedClass);
+//   } else {
+//     title.classList.add(clickedClass);
+//   }
+// }
 
 title.addEventListener("click", handleTitleClick);
-//title.onClick = handleTitleClick;
-title.addEventListener("mouseenter", handleMouseEnter);  
-title.addEventListener("mouseleave", handleMouseLeave);  
-
-window.addEventListener("resize", handleWindowResize);
-window.addEventListener("copy", handleWindowCopy);
-window.addEventListener("offline", handleWindowOffline);
-window.addEventListener("offline", handleWindowOnline);
-window.addEventListener("offline", handleWindowOffline);
